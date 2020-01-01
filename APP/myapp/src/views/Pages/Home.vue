4c886ea3b758a0161d4141608a2d3a5f43342147
KSBY867847-888 <xzj867847@sohu.com>
2020-01-01 20:17:26 +0800

    App

---
+++ APP/myapp/src/views/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        这是一个新的页面{{Mes}}
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    data(){
+        return {
+            Mes:"HelloWord"
+        }
+    },
+    methods:{},
+    mounted:function(){
+        console.log('页面开始吧了');
+    }
+}
+</script>
