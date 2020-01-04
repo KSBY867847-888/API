55424538775eb5ad2b922d61433e7017894e336e
KSBY867847-888 <xzj867847@sohu.com>
2020-01-05 00:06:47 +0800

    新增知识库

---
+++ 知识库/Jquery批量 上传/A---jQuery UpLoadify批量上传源码/Upload.ashx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Web;
+
+namespace Web
+{
+    /// <summary>
+    /// Upload 的摘要说明
+    /// </summary>
+    public class Upload : IHttpHandler
+    {
+        //51aspx下载
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpPostedFile file = context.Request.Files["FileData"];
+            string uploadpath = context.Server.MapPath(context.Request["folder"] + "\\");
+
+            if (file != null)
+            {
+                if (!Directory.Exists(uploadpath))
+                {
+                    Directory.CreateDirectory(uploadpath);
+                }
+                file.SaveAs(uploadpath + file.FileName);
+                context.Response.Write("1|222"); //标志位1标识上传成功，后面的可以返回前台的参数，比如上传后的路径等，中间使用|隔开
+            }
+            else
+            {
+                context.Response.Write("0|");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
